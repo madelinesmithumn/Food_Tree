5a6dd936ee379ca40c331dda8cc6852ba05d47d9
madelinesmithumn <smit8695@umn.edu>
2018-08-16 16:11:13 -0500

    makedhdryotu commandlline
    created new file

--- commandline/lib/checkdb_commandline.R
+++ commandline/lib/checkdb_commandline.R
@@ -37,3 +37,12 @@ check.db <- function(food_database_fn, food_records_fn, output_fn)
   write.table(foods.missing, output_fn, sep = "\t", quote = FALSE, row.names = FALSE)
   
 }
+
+#included the filter for these diet records because it uses the exact same files
+filter.db.by.diet.records <- function(food_database_fn, food_records_fn, output_fn)
+{
+  fdata <- read.table(food_database_fn, header = TRUE, sep="\t", colClasses="character", quote="", strip.white=T)
+  diet <- read.table(food_records_fn, header = TRUE, sep="\t", colClasses="character", quote="", strip.white=T)
+  valid_ids <- intersect(fdata$FoodID, diet$FoodID)
+  write.table(fdata[fdata$FoodID %in% valid_ids,], output_fn, sep="\t", quote=FALSE, row.names=FALSE)
+}
